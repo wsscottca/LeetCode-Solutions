930d545f1bcd6b045ed29cddcd5f091f8b9ce2dc
Will Scott <WSScottCA@gmail.com>
2022-12-01 15:58:45 -0800

    Time: 179 ms (74.37%), Space: 41.5 MB (72.77%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
