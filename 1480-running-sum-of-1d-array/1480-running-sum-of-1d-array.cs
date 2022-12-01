01ffb8c6ead723240d76f9e55d1dbbbafcab8259
Will Scott <WSScottCA@gmail.com>
2022-12-01 09:56:02 -0800

    Time: 252 ms (18.86%), Space: 41.6 MB (73.74%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public int[] RunningSum(int[] nums) {
-        int[] result = nums;
         int count = 0;
         for (int i = 0; i < nums.Length; i++)
         {
             count = count + nums[i];
-            result[i] = count;
+            nums[i] = count;
         }
-        return result;
+        return nums;
     }
 }
