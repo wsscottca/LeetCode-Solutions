8fa794b48b795f307d6b419a3ac4f4ff78f90619
Will Scott <WSScottCA@gmail.com>
2023-07-06 20:06:58 -0700

    Time: 56 ms (16.32%), Space: 16.2 MB (84.95%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,47 +1,17 @@
-#2611055971756562
-#                010
-#               110
-#              111
-#             111
-#            111
-#           111
-#          111
-#         211
-#        221
-#       222
-#      222
-#     222
-#    022
-#   202
-#  220
-# 222
-#422
-
-
 class Solution:
     def numDecodings(self, s: str) -> int:
         if s[0] == "0":
             return 0
         
         n = len(s)
-        
-        dp_last = 1
-        dp_two_prev = 0
-        curr = 0
+        dp = [0 for _ in range(n+1)]
+        dp[n] = 1
         
         for i in reversed(range(n)):
-            curr = 0
-            print(s[i])
             if s[i] != "0":
-                curr = dp_last
-            
-            if (i + 1 < n) and (s[i] == "1" or (s[i] == "2" and s[i+1] <= "6")):
-                curr += dp_two_prev
+                dp[i] = dp[i+1]
                 
-            print(f'curr: {curr}, last: {dp_last}, two_prev: {dp_two_prev}')
-
-            dp_two_prev = dp_last
-            dp_last = curr
-            
+            if (i < n - 1) and (s[i] == "1" or s[i] == "2" and s[i + 1] <= "6"):
+                dp[i] += dp[i + 2]
                 
-        return curr
+        return dp[0]
