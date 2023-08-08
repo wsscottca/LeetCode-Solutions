cb1f5761ce609c7d7a22f3a3c63367f54a7f94ee
Will Scott <WSScottCA@gmail.com>
2023-08-07 20:02:28 -0700

    Time: 55 ms (97.44%), Space: 20.7 MB (14.67%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,23 +1,36 @@
+
+
 class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.minStack = []
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        val = min(val, self.minStack[-1] if self.minStack else val)
-        self.minStack.append(val)
+        minimum = None
+        
+        # if the stack is empty, add the val and it is the min
+        if not self.stack:
+            self.stack.append((val, val))
+            return
+        
+        # otherwise check if the current min (the min from the last tuple) is greater than val
+        # if it is then set the new min (second index of tuple) to val
+        if val < self.stack[-1][1]:
+            minimum = val
+
+        else:
+            minimum = self.stack[-1][1]
+        self.stack.append((val, minimum))
+            
 
     def pop(self) -> None:
         self.stack.pop()
-        self.minStack.pop()
 
     def top(self) -> int:
-        return self.stack[-1]
+        return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.minStack[-1]
+        return self.stack[-1][1]
 
 
 # Your MinStack object will be instantiated and called as such:
