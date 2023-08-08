adf41b63f7c7f431b16c50dbfc62ce108204f42b
Will Scott <WSScottCA@gmail.com>
2023-08-07 20:30:11 -0700

    Time: 95 ms (96.75%), Space: 17.7 MB (86.69%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,9 +1,14 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         indegrees = [0 for _ in range(numCourses)]
+        outdegrees = {}
+        
+        for i in range(numCourses):
+            outdegrees[i] = []
         
         for prereq in prerequisites:
             indegrees[prereq[0]] += 1
+            outdegrees[prereq[1]].append(prereq[0])
         
         to_visit = []
         for i in range(numCourses):
@@ -14,11 +19,9 @@ class Solution:
         while(to_visit):
             node = to_visit.pop()
             topological.append(node)
-            for prereq in prerequisites:
-                if prereq[1] == node:
-                    indegrees[prereq[0]] -= 1
-                    if indegrees[prereq[0]] == 0:
-                        to_visit.append(prereq[0])
+            for neighbor in outdegrees[node]:
+                    indegrees[neighbor] -= 1
+                    if indegrees[neighbor] == 0:
+                        to_visit.append(neighbor)
         
-        print(topological)
         return len(topological) == numCourses
