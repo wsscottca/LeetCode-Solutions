8ff1d8628a44fe0006100e4acf785fda4cd8b8c5
Will Scott <WSScottCA@gmail.com>
2023-11-06 11:14:33 -0800

    Time: 45 ms (64.26%), Space: 18.6 MB (39.29%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -1,21 +1,40 @@
+'''
+Timestamps:
+    Understand and Cases:
+    Design and Verify:
+    Code:
+
+Understand and Cases:
+    root = [1,1,null,1,null,null,null]
+    root = [1]
+    root = [1,1,1,1,1,null,1]
+    
+Design and Verify:
+    DFS
+    
+    dfs(node):
+        if not node:
+            return 0
+        
+        height = max(node.left, node.right)
+        
+        return height + 1
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def _maxDepth(root: Optional[TreeNode], depth: int) -> int:
-    if not root:
-        return depth - 1
-    
-    left = _maxDepth(root.left, depth + 1)
-    right = _maxDepth(root.right, depth + 1)
-    
-    return max(left, right)
-        
-
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        return _maxDepth(root, 1)
-
+        # if there is no node, it cannot have a height
+        if not root:
+            return 0
         
+        # calculate the heights of each child subtree, and get the max
+        height = max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
+        # the current height is the max child's height plus the current node
+        return height + 1
