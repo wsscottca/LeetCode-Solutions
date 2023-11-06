e3efadfb6aa49e83dbb16a928e99874e3e1c7eb4
Will Scott <WSScottCA@gmail.com>
2023-11-05 16:24:21 -0800

    Time: 852 ms (64.21%), Space: 27.4 MB (29.16%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,19 +1,49 @@
+'''
+Timestamps:
+    Understand and Cases:
+    Design and Verify:
+    Code and Debug:
+    
+Understand and Cases:
+    prices = [1,1,1]
+    prices = []
+    prices = [2,1]
+    prices = [1,2]
+    prices = [1,2,3,4]
+    
+Design and Verify:
+    Initial Thought:
+        max var = 0
+        
+        if val at index 1 < val at index 0
+            buy = second element
+        else
+            buy = first element
+        
+        iterate, looking at the next value
+        
+        if it is lower - calculate current max
+            if current max is higher than max, set it
+            if the low is lower than our buy, set our buy to it
+        if it is higher or the same - ignore
+        
+        return max((last_item - buy), max)
+        
+        
+'''
+import sys
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0
-        left, right = 0, 1
-        while right < len(prices):
-            profit = prices[right] - prices[left]
-            maxProfit = max(profit, maxProfit)
+        highest_profit = 0
+        
+        # set the initial buy
+        buy = sys.maxsize
+        
+        # at each element check if we can have a lower buy
+        # or if selling is a higher profit
+        for price in prices:
+            buy = min(buy, price)
             
-            if prices[right] < prices[left]:
-                left = right
-                right += 1
-            else:
-                right += 1
+            highest_profit = max(highest_profit, (price - buy))
                 
-        return maxProfit
-            
-            
-                
-            
+        return highest_profit
