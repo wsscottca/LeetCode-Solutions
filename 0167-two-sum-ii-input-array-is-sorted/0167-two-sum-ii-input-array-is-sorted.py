d3a4b37633d0a4c7d79e58fc93eca99213c580ed
Will Scott <WSScottCA@gmail.com>
2023-12-07 13:33:22 -0800

    Time: 122 ms (41.58%), Space: 17.4 MB (7.45%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,48 @@
+'''
+Timestamps:
+    Cases: 0:47
+    Design: 4:18
+    Verify: 7:14
+    Code:
+
+Cases:
+    numbers = [1,2,3,5] target = 7
+    numbers = [1,2] target = 3
+    
+Design:
+    Dictionary using the indexes O(n) space and time
+    Two pointer O(n) time, O(1) space
+    
+                 l   r   sum = 7
+    numbers = [1,2,3,5] target = 7
+    
+    left = 0
+    right = len(numbers - 1)
+    
+    while numbers[left] + numbers[right] != target:
+        if numbers[left] + numbers[right] > target:
+            right -= 1
+        else:
+            left += 1
+    
+    return [left + 1, right + 1]
+'''
+
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left, right = 0 , len(numbers) - 1
-        while left < right:
-            if (numbers[left] + numbers[right] == target):
-                return [left + 1, right + 1]
-            elif (target - numbers[right] > numbers[left]):
-                left += 1
-            elif (target - numbers[left] < numbers[right]):
+        # pointer at the start and end
+        left = 0
+        right = len(numbers) - 1
+        
+        # until we find our target sum
+        while numbers[left] + numbers[right] != target:
+            # if the number is greater, move in the right bound
+            if numbers[left] + numbers[right] > target:
                 right -= 1
+            # if it is lower, move in the left bound
+            else:
+                left += 1
+
+        # as we're returning the number in line not index, add 1 to each so its 1 based not 0
+        return [left + 1, right + 1]
