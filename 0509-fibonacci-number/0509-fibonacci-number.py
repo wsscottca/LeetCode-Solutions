788fd7e0939ec04a8b4dbd3bc80eb958643f6c2a
Will Scott <WSScottCA@gmail.com>
2023-12-07 13:14:27 -0800

    Time: 42 ms (51.13%), Space: 16.3 MB (10.12%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,13 +1,41 @@
+'''
+Timestamps:
+    Understand and Cases: 0:51
+    Design and Verify: 6:19
+    Code:
+
+Cases:
+    n = 0
+    n = 1
+    n = 5
+
+Design and Verify:
+    Space optimized bottom-up DP
+    
+    prev = 0
+    two_prev = 1
+    
+    for i in range(1, n + 1):
+        curr = prev + two_prev
+        two_prev = prev
+        prev = curr
+    
+    return prev
+'''
+
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-
-        last_two = (1,1)
-        for i in range(2, n):
-            curr = last_two[0] + last_two[1]
-            last_two = (curr, last_two[0])
-
-        return last_two[0]
+        # intialize our starting values
+        prev = 0
+        two_prev = 1
+        
+        # for each int in between 1 and n (including n)
+        for i in range(1, n + 1):
+            # our current is the sum of our last two
+            curr = prev + two_prev
+            # then we update our last two
+            two_prev = prev
+            prev = curr
+        
+        # return prev as it was our most recent curr
+        return prev
