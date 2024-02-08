979ddf0729f2d7bbb5592d14bb03658c78ecb40b
Will Scott <WSScottCA@gmail.com>
2024-02-08 12:15:00 -0800

    Time: 38 ms (90.57%), Space: 19 MB (92.35%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -1,3 +1,43 @@
+'''
+Timestamps:
+    Cases: 1:20
+    Design: 3:00
+    Verify: 5:13
+    Code:
+    
+Understand and Cases:
+    head = [1,2,3] pos = [-1]
+    head = []
+
+Design and Verify:
+    Tortoise and hare
+    
+    # set our pointers
+    slow = head
+    fast = head
+    
+    # while they're not at the same point keep moving
+    while slow and fast:
+        slow = slow.next
+        fast = fast.next.next
+        
+        # once they hit each other move slow to the head and
+        # slowly increment both, once they hit again we have the start
+        # so return it
+        if slow == fast:
+            slow = head
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next
+            
+            return slow
+    
+    # if slow or fast = none there is no cycle
+    return None
+
+'''
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -6,20 +46,25 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # set our pointers
         slow = head
         fast = head
-        
+
+        # while they're not at the same point keep moving
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-            
+
+            # once they hit each other move slow to the head and
+            # slowly increment both, once they hit again we have the start
+            # so return it
             if slow == fast:
                 slow = head
-                
                 while slow != fast:
                     slow = slow.next
                     fast = fast.next
-                    
+
                 return slow
-        
+
+        # if slow or fast = none there is no cycle
         return None
